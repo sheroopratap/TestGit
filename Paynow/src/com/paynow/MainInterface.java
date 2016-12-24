7cb11dae4e5fe54df8da2afe617439812510bb03
Pratap <sheroopratap@gmail.com>
2016-12-24 13:17:23 +0530

    This is added for Amar Demo

---
+++ Paynow/src/com/paynow/MainInterface.java
@@ -0,0 +1,7 @@
+package com.paynow;
+
+/**
+ * Created by Pratap on 12/24/2016.
+ */
+public interface MainInterface {
+}
---
+++ Paynow/src/com/paynow/bankprocess/hdfc/HdfcProcess.java
@@ -0,0 +1,7 @@
+package com.paynow.bankprocess.hdfc;
+
+/**
+ * Created by Pratap on 12/24/2016.
+ */
+public class HdfcProcess {
+}
