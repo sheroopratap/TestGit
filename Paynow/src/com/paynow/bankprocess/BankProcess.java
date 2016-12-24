772e531d1447ed6127d5c9a1a6648249ed9b872e
Pratap <sheroopratap@gmail.com>
2016-12-24 14:21:28 +0530

    This is added by Ranjeet

--- Paynow/src/com/paynow/bankprocess/BankProcess.java
+++ Paynow/src/com/paynow/bankprocess/BankProcess.java
@@ -1,13 +1,36 @@
 package com.paynow.bankprocess;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Pratap on 12/24/2016.
  */
 public class BankProcess {
 
 
-    public void startProcess()
+    Map<String,List<String>> map = new HashMap<>();
+
+    public String startProcess()
     {
+        return null;
+    }
+
+    public List<String> getList(String key)
+    {
+        List<String> retList = map.get(key);
+
+        if(retList == null)
+            retList = new ArrayList<>();
+
+        return retList;
+
+    }
+    public void test()
+    {
+        List<String> list = getList("axy");
 
     }
 }
